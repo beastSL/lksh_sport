d8a1b153aac8fd36d25d8170c3ea7f368be4c2d5
SphericalPotatoInVacuum <a@bogdanov.co>
2019-07-22 01:20:06 +0400

    Fixed dropdown options problem

--- Client/js/index.js
+++ Client/js/index.js
@@ -1,4 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, options);
+document.addEventListener("DOMContentLoaded", function(event) {
+    var elems = document.querySelectorAll(".dropdown-trigger");
+    var instances = M.Dropdown.init(elems, {
+        hover: true,
+        constrainWidth: true,
+        coverTrigger: false
+    });
 });
